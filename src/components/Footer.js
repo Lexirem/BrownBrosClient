0f33c344969f4d0c119401d12e74bc500acaed12
Lexirem <me.vallribera@gmail.com>
2021-02-10 18:04:51 +0100

    pagina pedido

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,21 +9,22 @@ import { Link } from "react-router-dom";
                     <img className="logo" src="brown.png" alt="logo brown bros" />
                     <p>Tel.: </p>
                     <p>Email:</p>
-                    <a href="https://es-es.facebook.com/" title="Facebook" target="_blank">
+                    <a href="https://es-es.facebook.com/" title="Facebook" target="_blank" rel="noreferrer">
                         <img src="facebook.png" alt="Enlace a Facebook" />
                     </a>
-
-                    <a href="https://www.instagram.com/?hl=es" title="Instagram" target="_blank">
+                    <a href="https://www.instagram.com/?hl=es" title="Instagram" target="_blank" rel="noreferrer">
                         <img src="Instagram.png" alt="Enlace a Instagram" />
                     </a>
-
-                    <a href="https://twitter.com/?lang=es" title="Twitter"
-                    target="_blank">
+                    <a href="https://twitter.com/?lang=es" title="Twitter" target="_blank" rel="noreferrer">
                         <img src="twitter.png" alt="Enlace a twitter" />
                     </a>
-
                 </div>
-                
+                <div className="subNav">
+                    <p><Link to={""}>aaaaaaa</Link></p>
+                    <p><Link to={""}>aaaaaaa</Link></p>
+                    <p><Link to={""}>aaaaaaa</Link></p>
+                    <p><Link to={""}>aaaaaaa</Link></p>
+                </div>  
             </div>
         )
     }
